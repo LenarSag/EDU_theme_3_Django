2fc5fba688297f9391edfdff6271c1496caf3d65
LenarSag <saglenar@gmail.com>
2025-01-05 10:03:17 +0300

    added dog model

--- backend/dogs/models.py
+++ backend/dogs/models.py
@@ -1,3 +1,71 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from backend.core.constants import MAX_VALUE_ABILITIES, MIN_VALUE_ABILITIES
+
+
+class Breed(models.Model):
+    """Модель 'Порода'."""
+
+    class BreedSizes(models.TextChoices):
+        """Размеры породы."""
+
+        TINY = 'tiny'
+        SMALL = 'small'
+        MEDIUM = 'medium'
+        LARGE = 'large'
+
+    name = models.CharField(verbose_name='Название')
+    size = models.CharField(choices=BreedSizes.choices, verbose_name='Размер')
+    friendliness = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(MIN_VALUE_ABILITIES),
+            MaxValueValidator(MAX_VALUE_ABILITIES),
+        ],
+        verbose_name='Дружелюбие',
+    )
+    trainability = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(MIN_VALUE_ABILITIES),
+            MaxValueValidator(MAX_VALUE_ABILITIES),
+        ],
+        verbose_name='Обучаемость',
+    )
+    shedding_amount = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(MIN_VALUE_ABILITIES),
+            MaxValueValidator(MAX_VALUE_ABILITIES),
+        ],
+        verbose_name='Количество линьки',
+    )
+    exercise_needs = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(MIN_VALUE_ABILITIES),
+            MaxValueValidator(MAX_VALUE_ABILITIES),
+        ],
+        verbose_name='Потребность в активности',
+    )
+
+    class Meta:
+        verbose_name = 'Порода'
+        verbose_name_plural = 'Породы'
+        ordering = ('name',)
+
+
+class Dog(models.Model):
+    """Модель 'Собака'."""
+
+    name = models.CharField(verbose_name='Имя')
+    age = models.PositiveSmallIntegerField(verbose_name='Возраст')
+    breed = models.ForeignKey(
+        Breed, on_delete=models.SET_NULL, null=True, verbose_name='Порода'
+    )
+    gender = models.CharField(verbose_name='Пол')
+    color = models.CharField(verbose_name='Цвет')
+    favorite_food = models.CharField(null=True, verbose_name='Любимая еда')
+    favorite_toy = models.CharField(null=True, verbose_name='Любимая игрушка')
+
+    class Meta:
+        verbose_name = 'Собака'
+        verbose_name_plural = 'Собаки'
+        ordering = ('name',)
