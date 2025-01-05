1d921260048f30d5b03f96717f858b90154b7c37
LenarSag <saglenar@gmail.com>
2025-01-05 15:14:21 +0300

    add viewsets, urls, serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Avg, OuterRef, Subquery, Count
 from django.shortcuts import render
 
-from rest_framework import filters, mixins, status, viewsets
+from rest_framework import filters, status, viewsets
 from rest_framework.response import Response
 
 from backend.api.permissions import ReadOrAuthenticated, ReadOrAuthenticatedOrOwner
